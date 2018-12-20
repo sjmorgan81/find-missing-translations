cafdff136e4c65bec6c9b2ef812d0029fb04222c
Simon Morgan <simon.morgan@cereaddysg.org.uk>
2018-12-20 12:04:25 +0000

    Improve documentation

--- find-missing-translations.ps1
+++ find-missing-translations.ps1
@@ -1,17 +1,20 @@
-﻿$getTranslationRegex = "GetTranslation\(`"(?<TranslationKey>.+?)`"\)"
+﻿# Traverses the current directory searching for files that make use of GetTranslation and GetTranslationWithIcon
+
+$getTranslationRegex = "GetTranslation\(`"(?<TranslationKey>.+?)`"\)"
 $getTranslationWithIconRegex = "GetTranslationWithIcon\(`".+?`", ?`"(?<TranslationKey>.+?)`"\)"
 
 $existingTranslationKeys = New-Object 'System.Collections.Generic.HashSet[string]'
-$translationKeys = New-Object 'System.Collections.Generic.HashSet[string]'
+$usedTranslationKeys = New-Object 'System.Collections.Generic.HashSet[string]'
 
-# Load the file
+# Search for occurencdes of the specified regular expression in the specified file.
 function findRegexInPath($regex, $filePath) {
     Get-Content $filePath | Select-String -Pattern $regex -AllMatches | Select-Object -Expand Matches | ForEach-Object {
         $match = $_.Groups["TranslationKey"].Value
-        $translationKeys.Add($match) | Out-Null
+        $usedTranslationKeys.Add($match) | Out-Null
     }
 }
 
+# Load translation keys from the specified file.
 function loadExistingTranslationKeys($filePath) {
     [xml]$XmlDocument = Get-Content $filePath
     $XmlDocument.root.data | ForEach-Object {
@@ -31,6 +34,7 @@ foreach ($filePath in $args) {
     loadExistingTranslationKeys $filePath
 }
 
+# Recursively search the current directory searching for files that make use of translations.
 Get-ChildItem -Recurse -Include "*.aspx", "*.ascx" | ForEach-Object {
     $filePath = $_.FullName
     Write-Output "Searching for translation keys in $($filePath)"
@@ -40,7 +44,7 @@ Get-ChildItem -Recurse -Include "*.aspx", "*.ascx" | ForEach-Object {
 
 Write-Host "The following translation keys seem to be missing:" -BackgroundColor Red -ForegroundColor White
 # Check whether the keys we've found exist in the resource files.
-$translationKeys | ForEach-Object {
+$usedTranslationKeys | ForEach-Object {
     if (!$existingTranslationKeys.Contains($_)) {
         Write-Output $_
     }
