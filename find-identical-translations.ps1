2e11c692e9e85327a62040d5c13fa97fd94b9975
Simon Morgan <simon.morgan@ceredigion.gov.uk>
2024-02-23 12:03:41 +0000

    Restore original script to find missing translations

---
+++ find-identical-translations.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+
+if ($args.Count -ne 2) {
+    Write-Output "Please specify some resource files."
+    Write-Output "Usage: $($MyInvocation.ScriptName) file1 file2"
+    exit -1
+}
+
+# Define the paths to the two .resx files
+$resxFile1 = $args[0]
+$resxFile2 = $args[1]
+
+# Load the System.Windows.Forms assembly to access the ResXResourceSet class
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create ResXResourceSet objects for both files
+$resourceSet1 = New-Object System.Resources.ResXResourceSet -ArgumentList $resxFile1
+$resourceSet2 = New-Object System.Resources.ResXResourceSet -ArgumentList $resxFile2
+
+# Get all the keys from both resource sets
+$keys1 = $resourceSet1.GetEnumerator() | ForEach-Object { $_.Key }
+$keys2 = $resourceSet2.GetEnumerator() | ForEach-Object { $_.Key }
+
+foreach ($key in $keys1) {
+    $value1 = $resourceSet1.GetString($key)
+    $value2 = $resourceSet2.GetString($key)
+
+    if (-not $keys2.Contains($key)) {
+        Write-Host "MISSING: '$key' is missing from $resxFile2"
+    }
+    elseif ($value1 -eq $value2) {
+        Write-Host "IDENTICAL: '$key' has the same value in both files"
+    }
+}
+
+foreach ($key in $keys2) {
+    $value1 = $resourceSet1.GetString($key)
+    $value2 = $resourceSet2.GetString($key)
+
+    if (-not $keys1.Contains($key)) {
+        Write-Host "MISSING: '$key' is missing from $resxFile1"
+    }
+}
+
+# Clean up the resource sets
+$resourceSet1.Close()
+$resourceSet2.Close()
